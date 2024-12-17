c400fa482ab803968af9baf0df76d270b4e02150
Juanma Molins Cortés <35576283+jmmolins87@users.noreply.github.com>
2024-12-17 17:50:52 +0100

    wip navbar and any tokens

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -6,11 +6,13 @@ import { TranslateModule } from '@ngx-translate/core';
 
 import { NavBarComponent } from './components/nav-bar/nav-bar.component';
 import { PrimeNgModule } from '../prime-ng/prime-ng.module';
+import { FooterComponent } from './components/footer/footer.component';
 
 
 @NgModule({
   declarations: [
-    NavBarComponent
+    NavBarComponent,
+    FooterComponent
   ],
   imports: [
     CommonModule,
