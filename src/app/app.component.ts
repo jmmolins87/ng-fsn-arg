5eebc1eaf694c05004353af64c1784c1080405ee
Juanma Molins Cortés <35576283+jmmolins87@users.noreply.github.com>
2024-12-18 17:44:07 +0100

    wip

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,9 +9,7 @@ import { PrimeNGConfig } from 'primeng/api';
 })
 export class AppComponent implements OnInit {
   
-
-
-  constructor(private primengConfig: PrimeNGConfig) { }
+  constructor( private primengConfig: PrimeNGConfig ) { }
 
   ngOnInit() {
     // Ripple effect
